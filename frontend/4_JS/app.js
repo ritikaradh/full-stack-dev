eec2a9ca5e4d11539145c9f2eb3cf5588733ee05
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-04-25 22:21:59 +0530

    JS Programming Basics

---
+++ frontend/4_JS/app.js
@@ -0,0 +1,14 @@
+console.log("Hello World!");
+console.log("Ritika Upadhyay");
+
+let a=2;
+let b=5;
+
+console.log("The sum is: ", a+b);
+
+let pencilPrice = 10;
+let eraserPrice = 5;
+
+let output = "The total price is: " + (pencilPrice + eraserPrice) + " Rupees." ;
+
+console.log(output);
