7e853f42df924d290e30c9eee8552c1c0e5f40bc
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-04-28 16:36:36 +0530

    String Methods Part 1 done.

---
+++ frontend/4_JS/PracticeQuestion5.js
@@ -0,0 +1,18 @@
+// 1. For the given string:
+//     let msg = "help!";
+//  Trim it and convert it to uppercase.
+
+let msg = "help!";
+newMsg = msg.trim().toUpperCase();
+console.log(newMsg);
+
+// 2. For the string -> let namee = "Apna College", predict the output for the following:
+//     namee.slice(4,9)
+//     namee.indexOf("na")
+//     namee.replace("Apna", "Our")
+
+let namee = "ApnaCollege";
+
+console.log(namee.slice(4,9));
+console.log(namee.indexOf("na"));
+console.log(namee.replace("Apna", "Our"));
