863936fcab979dfdd64428982d3caec2d2f8272c
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-05-01 16:31:53 +0530

    Multiplication table of 5

--- frontend/4_JS/PracticeQuestion8.js
+++ frontend/4_JS/PracticeQuestion8.js
@@ -1,20 +1,27 @@
 // 1.  Print all odd numbers in the range 1-15 using for loop.
 
-let start = 1;
-let end = 15;
+// let start = 1;
+// let end = 15;
 
-for(i=start; i <= end; i++){
-    if (i % 2 != 0){
-        console.log(i);
-    }
-}
+// for(i=start; i <= end; i++){
+//     if (i % 2 != 0){
+//         console.log(i);
+//     }
+// }
 
-// 2.  Print all even numbers in the range 1-15 using for loop.
+// // 2.  Print all even numbers in the range 1-15 using for loop.
 
-console.log("Printing even numbers");
+// console.log("Printing even numbers");
 
-for(i=start; i <= end; i++){
-    if (i % 2 == 0){
-        console.log(i);
-    }
+// for(i=start; i <= end; i++){
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+//3.    Print the multiplication table of five.
+
+let n = 5;
+for (let i= 1; i <= 10; i++){
+    console.log(`${n} * ${i} = ${n * i}`);
 }
