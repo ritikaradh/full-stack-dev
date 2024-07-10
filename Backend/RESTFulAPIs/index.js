fe285da581ea549d7b67b82a75384458cb2df53d
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-07-10 16:22:35 +0530

    RESTful APIs done!

--- Backend/RESTFulAPIs/index.js
+++ Backend/RESTFulAPIs/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4 : uuidv4 } = require("uuid");
 const port = 8080;
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'))
+
 
 app.listen(port, ()=>{
     console.log("listening to port: 8080");
@@ -18,17 +22,17 @@ app.get("/", (req, res)=>{
 
 let posts = [
     {
-        id: "5a",
+        id: uuidv4(),
         username: "@tisha",
         status: "Working Hard to Achieve Dreams!"
     },
     {
-        id: "5b",
+        id: uuidv4(),
         username: "@ritika",
         status: "I will succeed!",
     },
     {
-        id: "5c",
+        id: uuidv4(),
         username: "@teamsrishti",
         status: "We will Build Fashion Spy!"
     }
@@ -46,6 +50,7 @@ app.get("/posts/new", (req, res) => {
 app.post("/posts", (req, res) => {
     let { username, status } = req.body;
     let newPost = {
+        id: uuidv4(),
         username: username,
         status: status
     };
@@ -60,3 +65,30 @@ app.get("/posts/:id", (req,res)=>{
     console.log(post);
     res.render("show.ejs", {post});
 });
+
+//patch route
+app.patch("/posts/:id", (req, res)=> {
+    let { id } = req.params;
+    let newStatus = req.body.status;
+    let post = posts.find( (p) => id  === p.id);
+    post.status = newStatus;
+    console.log(post);
+    res.redirect("/posts")
+    // res.send("ok.")
+});
+
+//update route
+app.get("/posts/:id/edit", (req, res)=>{
+    let { id } = req.params;
+    let post = posts.find( (p) => id === p.id);
+    res.render("edit.ejs", { post });
+});
+
+//destroy route
+app.delete("/posts/:id", (req, res)=>{
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+});
+
+
